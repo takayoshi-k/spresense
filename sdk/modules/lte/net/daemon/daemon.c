e234018957d82af0d1416eb36be4d0e1a42d1d78
Yutaka Miyajima <Yutaka.x.Miyajima@sony.com>
2021-02-19 16:55:14 +0900

    Revert "lte: altcom: Fix bug that send failed after modem reset and restart callback was not called"
    This reverts commit bd9fc80d898d3ee1162ac5b1c216a55a2602022f.
    
    refs #12748

--- sdk/modules/lte/net/daemon/daemon.c
+++ sdk/modules/lte/net/daemon/daemon.c
@@ -2832,7 +2832,7 @@ static int daemon_api_request(int read_fd, int usock_fd,
         break;
 
       case DAEMONAPI_REQUEST_POWER_ON:
-        priv->poweron_result = altcom_power_on(LTE_RESTART_USER_INITIATED);
+        priv->poweron_result = altcom_power_on();
         if (priv->poweron_result < 0)
           {
             priv->poweron_inprogress = false;
